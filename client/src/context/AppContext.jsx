c1e120c366af4fdcaccbeccbe2614ee0c4933a38
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-01-16 03:02:27 +0300

    AppContext

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,32 @@
+import {createContext, useState} from "react";
+import {toast} from "react-toastify";
+
+import axios from "axios";
+import {data} from "autoprefixer";
+
+export const AppContent = createContext()
+export const AppContextProvider = (props) =>{
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userData, setUserData] = useState(false)
+
+    const getUserData = async () => {
+        try{
+            const {data} = await axios.get(backendUrl + "/api/user/data")
+            data.success ? setUserData(data.userData) : toast.error(data.message)
+        }catch(error){
+            toast.error(data.message)
+        }
+    }
+    const value ={
+        backendUrl,
+        isLoggedIn, setIsLoggedIn,
+        userData, setUserData,
+        getUserData
+    }
+    return (
+        <AppContent.Provider value={value}>
+            {props.children}
+        </AppContent.Provider>
+    )
+}
